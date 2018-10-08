5bcb54a198c9094083e2e13bb381c6d92210e895
Anonymous <anonymous@email.com>
2018-10-08 23:33:06 +1100

    describe a couple of essential functions for the app

--- src/toy_robot/core.clj
+++ src/toy_robot/core.clj
@@ -1,6 +1,13 @@
 (ns toy-robot.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn play
+  "Plays a sequence of commands on a board with specified state to start with, and returns final state."
+  [cmd-seq start-state])
+
+(defn file->commands
+  "Returns a sequence of commands from parsing a text file containing commands, one command per line."
+  [filepath])
+
+(defn stdin->commands
+  "Returns a sequence of commands by parsing command inputs from standard input."
+  [])
