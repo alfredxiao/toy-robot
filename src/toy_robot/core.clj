ae1a396e226bff83ddc826fc031f1e95e12665bd
Anonymous <anonymous@email.com>
2018-10-09 00:05:21 +1100

    draft ideas of how the play function can work with each play step is interpreted by the `go` function which dispatches based on command type

--- src/toy_robot/core.clj
+++ src/toy_robot/core.clj
@@ -1,8 +1,42 @@
 (ns toy-robot.core)
 
+; Example state: {:x 0, :y 0, :face :EAST}
+; All commands:
+; {:command :LEFT} {:command :PLACE, :args {:x 0, :y 0, :face :EAST}}}
+; Valid commands : LEFT, RIGHT, MOVE, REPORT, PLACE
+
+
+(defmulti go
+          "Plays one go by interpreting provided command and its optional arguments and transition
+          state or keep it in cases like a fall-over or invalid command, maybe"
+          :command)
+
+(defmethod go :LEFT
+  [state _])
+
+(defmethod go :RIGHT
+  [state _])
+
+(defmethod go :MOVE
+  [state _])
+
+(defmethod go :REPORT
+  [state _])
+
+(defmethod go :PLACE
+  [_ command])
+
+(defmethod go :default
+  [state]
+  state)
+
 (defn play
   "Plays a sequence of commands on a board with specified state to start with, and returns final state."
-  [cmd-seq start-state])
+  [cmd-seq state]
+  (reduce
+    go
+    state
+    cmd-seq))
 
 (defn file->commands
   "Returns a sequence of commands from parsing a text file containing commands, one command per line."
