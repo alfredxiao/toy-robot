1d648b40a44076e9ef4bab206cc699a01cdec1cd
Anonymous <anonymous@email.com>
2018-10-09 22:15:36 +1100

    implements one-step move

--- src/toy_robot/core.clj
+++ src/toy_robot/core.clj
@@ -7,24 +7,32 @@
 
 (def faces [:EAST :SOUTH :WEST :NORTH])
 
-(def left (zipmap faces (drop 3 (cycle faces))))   ; ==> {:EAST :NORTH, :SOUTH :EAST, :WEST :SOUTH, :NORTH :WEST}
-(def right (zipmap faces (drop 1 (cycle faces))))  ; ==> {:EAST :SOUTH, :SOUTH :WEST, :WEST :NORTH, :NORTH :EAST}
-
 (defmulti go
           "Plays one go by interpreting provided command and its optional arguments and transition
           state or keep it in cases like a fall-over or invalid command, maybe"
           (fn [state command] (:command command)))
 
+(def left (zipmap faces (drop 3 (cycle faces))))   ; ==> {:EAST :NORTH, :SOUTH :EAST, :WEST :SOUTH, :NORTH :WEST}
 (defmethod go :LEFT
   [state _]
   (update state :face left))
 
+(def right (zipmap faces (drop 1 (cycle faces))))  ; ==> {:EAST :SOUTH, :SOUTH :WEST, :WEST :NORTH, :NORTH :EAST}
 (defmethod go :RIGHT
   [state _]
   (update state :face right))
 
+(def displacements {:EAST  {:x 1,  :y  0}
+                    :SOUTH {:x 0,  :y -1}
+                    :WEST  {:x -1, :y  0}
+                    :NORTH {:x 0,  :y  1}})
+
 (defmethod go :MOVE
-  [state _])
+  [state _]
+  (let [displacement (displacements (:face state))]
+    (-> state
+        (update :x + (:x displacement))
+        (update :y + (:y displacement)))))
 
 (defmethod go :REPORT
   [state _]
