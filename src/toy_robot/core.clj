439778b0fa5f00fedfac6660f6f948790fe2619b
Anonymous <anonymous@email.com>
2018-10-09 23:03:22 +1100

    make sure commands are ignored until a valid PLACE happens

--- src/toy_robot/core.clj
+++ src/toy_robot/core.clj
@@ -1,24 +1,24 @@
 (ns toy-robot.core)
 
-; Example state: {:x 0, :y 0, :face :EAST}
+; Example state: {} {:x 0, :y 0, :face :EAST :on-table? true}
 ; All commands:
 ; {:command :LEFT} {:command :PLACE, :args {:x 0, :y 0, :face :EAST}}}
 ; Valid commands : LEFT, RIGHT, MOVE, REPORT, PLACE
 
 (def faces [:EAST :SOUTH :WEST :NORTH])
 
-(defmulti go
-          "Plays one go by interpreting provided command and its optional arguments and transition
-          state. This function does not prevent the robot from falling, that is done in `safe-go`"
+(defmulti execute
+          "Execute one step by interpreting provided command and its optional arguments and transition
+          state. This function does not prevent the robot from falling, that is done in `safe-execute`"
           (fn [state command] (:command command)))
 
 (def left (zipmap faces (drop 3 (cycle faces))))   ; ==> {:EAST :NORTH, :SOUTH :EAST, :WEST :SOUTH, :NORTH :WEST}
-(defmethod go :LEFT
+(defmethod execute :LEFT
   [state _]
   (update state :face left))
 
 (def right (zipmap faces (drop 1 (cycle faces))))  ; ==> {:EAST :SOUTH, :SOUTH :WEST, :WEST :NORTH, :NORTH :EAST}
-(defmethod go :RIGHT
+(defmethod execute :RIGHT
   [state _]
   (update state :face right))
 
@@ -27,24 +27,24 @@
                     :WEST  {:x -1, :y  0}
                     :NORTH {:x 0,  :y  1}})
 
-(defmethod go :MOVE
+(defmethod execute :MOVE
   [state _]
   (let [displacement (displacements (:face state))]
     (-> state
         (update :x + (:x displacement))
         (update :y + (:y displacement)))))
 
-(defmethod go :REPORT
+(defmethod execute :REPORT
   [state _]
   (do
     (println state)
     state))
 
-(defmethod go :PLACE
+(defmethod execute :PLACE
   [_ command]
   (:args command))
 
-(defmethod go :default
+(defmethod execute :default
   [state _]
   state)
 
@@ -52,19 +52,29 @@
   (and (<= 0 x 4)
        (<= 0 y 4)))
 
-(defn- safe-go [state command]
-  (let [new-state (go state command)]
+(defn- safe-execute [state command]
+  (let [new-state (execute state command)]
     (if (valid? new-state)
       new-state
       state)))
 
+(defn- go [state command]
+  (cond
+    (:on-table? state)
+    (safe-execute state command)
+
+    (= :PLACE (:command command))
+    (let [new-state (safe-execute state command)]
+      (if (= new-state state)
+        state
+        (assoc new-state :on-table? true)))
+
+    :else state))
+
 (defn play
   "Plays a sequence of commands on a board with specified state to start with, and returns final state."
-  [cmd-seq state]
-  (reduce
-    safe-go
-    state
-    cmd-seq))
+  [cmd-seq]
+  (reduce go {:on-table? false} cmd-seq))
 
 (defn file->commands
   "Returns a sequence of commands from parsing a text file containing commands, one command per line."
