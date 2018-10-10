0666f856612aeebc4cecaaa1c220af86702db02f
Anonymous <anonymous@gmail.com>
2018-10-10 22:37:53 +1100

    added strong validation for PLACE command, such that x,y,face are all validated

--- src/toy_robot/parser.clj
+++ src/toy_robot/parser.clj
@@ -1,9 +1,6 @@
 (ns toy-robot.parser
   (:require [clojure.string :refer [trim split upper-case]]))
 
-;; to-improve:
-;;   validate number of arguments
-;;   validate that x,y are integer values, face are either EAST, SOUTH, WEST, NORTH
 (defn line->command
   "Parses a command line and returns a command map, or nil if invalid"
   [line]
@@ -13,11 +10,19 @@
       (let [[_ cmd args-str] (re-matches #"(LEFT|RIGHT|MOVE|REPORT|PLACE)(?:\s+(.+))?" trimmed)]
         (when-not (empty? cmd)
           (if (= cmd "PLACE")
-            (when-let [[x y face] (map trim (split args-str #","))]
-              {:command :PLACE
-               :args {:x (Integer/parseInt x)
-                      :y (Integer/parseInt y)
-                      :face (keyword face)}})
+            (let [[x y face & extras] (when args-str
+                               (map trim (split args-str #",")))]
+              (when (and (empty? extras)
+                         (not (empty? x))
+                         (re-find #"\d" x)
+                         (not (empty? y))
+                         (re-find #"\d" y)
+                         (contains? #{"EAST" "SOUTH" "WEST" "NORTH"}
+                                    face))
+                {:command :PLACE
+                 :args {:x (Integer/parseInt x)
+                        :y (Integer/parseInt y)
+                        :face (keyword face)}}))
             {:command (keyword cmd)}))))))
 
 (defn lines->commands
