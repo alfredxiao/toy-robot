7f76ba689115751f22c50931f9d3d2b8ec6e8ec9
Anonymous <anonymous@gmail.com>
2019-11-10 18:49:26 +1100

    update with new versions of deps

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]]
-  :plugins [[coverlet "0.1.7"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :plugins [[coverlet "0.1.8"]]
   :profiles {:dev {:source-paths   ["dev/src"]}}
   :main toy-robot.main)
